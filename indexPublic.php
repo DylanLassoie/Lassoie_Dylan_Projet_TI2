f522ce5e6d1433b608e98bd12951544cc2ef3cc2
DylanLassoie <dylan.lassoie@condorcet.be>
2022-04-29 10:04:45 +0200

    Ajout de l'index de la partie public

---
+++ indexPublic.php
@@ -0,0 +1,17 @@
+<?php
+    session_start();
+    require './admin/lib/php/admin_liste_include.php';
+?>
+
+<!doctype html>
+<html>
+    <head>
+
+    </head>
+    <body>
+
+    </body>
+    <footer>
+
+    </footer>
+</html>
